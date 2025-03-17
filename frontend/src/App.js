ec375d4fc58933b05fc225d81634d81ca033b8fd
viki-terebova <160360978+viki-terebova@users.noreply.github.com>
2025-03-17 23:41:17 +0100

    added start page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import Button from "./components/Button";
+import "./style.css";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const handlePlay = () => {
+    console.log("Playing");
+  }
 
-  useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error("Error fetching data:", error));
-  }, []);
-
-  return <h1>{message || "Loading..."}</h1>;
+  return (
+    <div className="start-page">
+      <h1 className="start-welcome-text">Welcome to FiniAutoma</h1>
+      <Button text="Play" onClick={handlePlay} className="start-button"/>
+    </div>
+  );
 }
 
 export default App;

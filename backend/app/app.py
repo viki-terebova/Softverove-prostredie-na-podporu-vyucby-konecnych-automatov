69609bf3737a6c10e28ecf66426a1d0a5128eef8
viki-terebova <160360978+viki-terebova@users.noreply.github.com>
2025-04-02 00:05:22 +0200

    running app on one server port

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,20 +1,31 @@
-from flask import jsonify
-from app import app, db
+from flask import Flask, send_from_directory, jsonify
+from flask_cors import CORS
+from database.db_provider import DBProvider 
+import os
 
-# for recognizing the db package
-from database.db_provider import DBProvider
-db: DBProvider
+app = Flask(__name__, static_folder="../../frontend/build", static_url_path="/")
+CORS(app)
 
-@app.route("/")
-def home():
-    return jsonify({"message": "Welcome to FiniAutoma!"})
+db = DBProvider()
 
-@app.route("/users", methods=["GET"])
-def get_users():
-    print("get_users")
+from app.app import *
+
+# React static files
+@app.route("/", defaults={"path": "index.html"})
+
+@app.route("/<path:path>", methods=["GET"])
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
+
+# API endpoints
+@app.route("/get_categories", methods=["GET"])
+def get_categories():
     categories = db.get_categories_for_user()
 
     if categories is None:
         return jsonify({"error": "Database error"}), 500
-
-    return jsonify(categories)
+    return categories
